3303a911249a4e3d7d00da304977cc8164ffd498
Alex <alex.pober.95@gmail.com>
2025-07-29 00:44:39 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from public directory (React build)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Azure configuration
 const AZURE_OPENAI_ENDPOINT = process.env.AZURE_OPENAI_ENDPOINT;
